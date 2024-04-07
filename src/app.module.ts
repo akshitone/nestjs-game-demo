1092efc1997037e3720f99648a6e55686392e8b6
akshitone <akshitone8@gmail.com>
2024-04-07 14:43:03 +0530

    LN-03 removed unnecessary files of app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,11 @@
 import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { SongsModule } from './songs/songs.module';
 import { LoggerMiddleware } from './common/middleware/logger/logger.middleware';
 
 @Module({
   imports: [SongsModule],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
